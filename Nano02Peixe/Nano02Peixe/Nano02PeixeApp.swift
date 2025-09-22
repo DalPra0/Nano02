e6ec09c742f6deec2e09eefbf9892fe4d241690f
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2025-09-22 16:49:44 -0300

    Initial project setup for Nano02Peixe app
    Add Xcode project files, asset catalogs, and initial SwiftUI files (ContentView and Nano02PeixeApp) for the Nano02Peixe iOS application.

---
+++ Nano02Peixe/Nano02Peixe/Nano02PeixeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nano02PeixeApp.swift
+//  Nano02Peixe
+//
+//  Created by Lucas Dal Pra Brascher on 22/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Nano02PeixeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
